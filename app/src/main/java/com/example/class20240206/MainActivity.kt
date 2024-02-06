84b8bf4cb1da2209c8ac4fc99cc9ab2849c8437d
Meerkat1030 <hong991000@gmail.com>
2024-02-06 16:36:37 +0900

    [COMMIT] 02/06 commit kotlin practice

---
+++ app/src/main/java/com/example/class20240206/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.class20240206
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import com.example.class20240206.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_test, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
